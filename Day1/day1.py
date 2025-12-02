581c133a925ead2e3efd04b5a3abd531580ff11a
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-12-02 09:20:39 +0100

    feat: day1 question set

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,1 +1,27 @@
-print(3 + 5)
+# Questions 
+# Exercises - Day 1
+# 1. Check the python version you are using
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#       addition(+)
+#       subtraction(-)
+#       multiplication(*)
+#       modulus(%)
+#       division(/)
+#       exponential(**)
+#       floor division operator(//)
+# 3. Write strings on the python interactive shell. The strings are the following:
+#       Your name
+#       Your family name
+#       Your country
+#       I am enjoying 30 days of python
+# 4. Check the data types of the following data:
+#       10
+#       9.8
+#       3.14
+#       4 - 4j
+#       ['Asabeneh', 'Python', 'Finland']
+#       Your name
+#       Your family name
+#       Your country
+# 5. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+# 6. Find an Euclidian distance between (2, 3) and (10, 8)
