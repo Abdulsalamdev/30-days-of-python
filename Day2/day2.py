b04117a62c9ed5822a6bdce10ce4393c35867ca9
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-12-15 10:26:22 +0100

    feat: completed day 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,32 +1,63 @@
-# Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
 # Write a python comment saying 'Day 2: 30 Days of python programming'
 # Declare a first name variable and assign a value to it
+first_name = "Silent"
 # Declare a last name variable and assign a value to it
+last_name="Architect"
 # Declare a full name variable and assign a value to it
+full_name = "Silent Architect"
 # Declare a country variable and assign a value to it
+country = "Nigeria"
 # Declare a city variable and assign a value to it
+city = "Akure"
 # Declare an age variable and assign a value to it
+age= 19
 # Declare a year variable and assign a value to it
+year = 2025
 # Declare a variable is_married and assign a value to it
+is_married = False
 # Declare a variable is_true and assign a value to it
+is_true = True
 # Declare a variable is_light_on and assign a value to it
+is_light_on = False
 # Declare multiple variable on one line
+first_name,last_name,age,is_married = "Abdulsalam","akinyoola",19, True
 # Exercises: Level 2
 # Check the data type of all your variables using type() built-in function
+print(type(first_name))
 # Using the len() built-in function, find the length of your first name
+print(len(first_name))
 # Compare the length of your first name and your last name
+print(len(first_name) == len(last_name))
 # Declare 5 as num_one and 4 as num_two
+num_one = 5
+num_two = 4
 # Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
 # Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
 # Multiply num_two and num_one and assign the value to a variable product
+product = num_one * num_two
 # Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
 # Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_one % num_two
 # Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
 # Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
 # The radius of a circle is 30 meters.
+raduis = 30
 # Calculate the area of a circle and assign the value to a variable name of area_of_circle
+area_of_circle = 22/7 * raduis * raduis
 # Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = 2 * 22/7 * raduis
 # Take radius as user input and calculate the area.
-# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+raduis = int(input("Enter the radius: "))
+# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable 
+first_name = input("Enter First Name")
+last_name =input("Enter last name")
+country = input("Enter Your country")
+age = int(input("enter your age"))
 # Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
+help("keywords")
 # 🎉 CONGRATULATIONS ! 🎉
